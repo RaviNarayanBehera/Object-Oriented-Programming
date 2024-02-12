04c052d29dea9a70424a14a96751be699da9a830
Ravi Narayan Behera <148530184+RaviNarayanBehera@users.noreply.github.com>
2024-02-12 15:25:57 +0530

    Add files via upload

---
+++ PR.3 Heritage/Program to demonstrate an example of hierarchical inheritance to get the square and cube of a number.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+using namespace std;
+
+class Main
+{
+	protected :
+		int a;
+		
+	public :
+		void input()
+		{
+			cout<<"Enter no.: ";
+			cin>>a;
+		}
+};
+class Square : public Main
+{
+	public :
+		void get()
+		{
+			input();
+			cout<<"Square : "<<a*a;	
+		}
+};
+class Cube : public Main
+{
+	public :
+	void get()
+	{
+		cout<<endl<<endl;
+		input();
+		cout<<"Cube : "<<a*a*a;	
+	}	
+};
+
+int main()
+{
+	Square s1;
+	Cube c1;
+	
+	s1.get();
+	c1.get();
+}
