21a6003fd44e0ca09c9ed01ceac8d2c7f84c5f10
Ravi Narayan Behera <148530184+RaviNarayanBehera@users.noreply.github.com>
2024-02-21 19:37:23 +0530

    Add files via upload

--- Lecture 6.2/1. Pattern.cpp
+++ Lecture 6.2/1. Pattern.cpp
@@ -4,25 +4,23 @@ using namespace std;
 main() 
 {
     for(int i=0; i<5; i++) 
-	{
+    {
         for(int j=0; j<=i; j++) 
-		{
+	{
             int x = 0;
             for(int k=0; k<j; k++) 
-			{
+	    {
                 x = x + 5 - k;
             }
             if(j%2==0) 
-			{
+	    {
                 cout << x + i - j + 1 << " ";
             } 
-			else
-			{
+	    else
+	    {
                 cout << x + 5 - i << " ";
             }
         }
         cout << endl;
     }
 }
-
-
